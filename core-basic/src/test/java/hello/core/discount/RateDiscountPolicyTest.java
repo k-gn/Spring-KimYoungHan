76d483e5da941cd6a9b497cd0ef4c64002c22ceb
k-gn <rlarbska97@gmail.com>
2022-10-09 21:33:35 +0900

    feat: 할인율 변경 (FIX -> RATE)

---
+++ core-basic/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,37 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    void vip_x() {
+        // given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
