18186f41695a6a2778b9404dd0790f431cfdb761
k-gn <rlarbska97@gmail.com>
2022-11-30 20:17:58 +0900

    fix: update line

--- core-advance/src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalService.java
+++ core-advance/src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalService.java
@@ -8,7 +8,8 @@ public class ThreadLocalService {
 	private ThreadLocal<String> nameStore = new ThreadLocal<>();
 
 	public String logic(String name) {
-		log.info("저장 name={} -> nameStore={}", name, nameStore.get()); nameStore.set(name);
+		log.info("저장 name={} -> nameStore={}", name, nameStore.get());
+		nameStore.set(name);
 		sleep(1000);
 		log.info("조회 nameStore={}",nameStore.get());
 		return nameStore.get();
