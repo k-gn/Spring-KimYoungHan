8d26ccaad8dc9f60b275785bd900a24e6899dde7
k-gn <rlarbska97@gmail.com>
2022-12-05 18:43:02 +0900

    chore: jpa basic project init

---
+++ jpa-basic/src/test/java/com/ex1/jpa_basic/JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex1.jpa_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
