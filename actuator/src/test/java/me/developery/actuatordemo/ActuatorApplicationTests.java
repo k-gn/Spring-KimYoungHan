da73011d931219fda1c579bb70a4717ee42bfc45
k-gn <rlarbska97@gmail.com>
2023-06-05 10:38:46 +0900

    chore: start actuator

---
+++ actuator/src/test/java/me/developery/actuatordemo/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.developery.actuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
