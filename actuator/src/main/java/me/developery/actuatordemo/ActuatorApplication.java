a0b0c8b463bd60a0fd23eaa87c00ddc6396b0866
k-gn <rlarbska97@gmail.com>
2023-06-06 11:01:18 +0900

    feat: update endpoints

--- actuator/src/main/java/me/developery/actuatordemo/ActuatorApplication.java
+++ actuator/src/main/java/me/developery/actuatordemo/ActuatorApplication.java
@@ -3,6 +3,10 @@ package me.developery.actuatordemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	# spring boot auto configuration
+	- @ConditionalOne~ 애노테이션을 활용하여 해당 기능에 대한 의존성이 있냐 없냐에 따라 빈을 생성해주거나 생성하지 않는다.
+ */
 @SpringBootApplication
 public class ActuatorApplication {
 
